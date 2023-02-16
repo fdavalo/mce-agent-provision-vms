ea77ca4145ac3c38dac954fc2d004970eb17040c
fdavalo <57140104+fdavalo@users.noreply.github.com>
2023-02-16 13:46:45 +0100

    Update update-agents.sh

--- scripts/update-agents.sh
+++ scripts/update-agents.sh
@@ -1,5 +1,9 @@
+#install jq
+curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
+chmod +x jq
+
 #fetch nodes from terraform.tfstate
-jq -r '.resources[] | select(.name=="ocp_node") | .instances[].attributes.name' terraform.tfstate > /tmp/res
+./jq -r '.resources[] | select(.name=="ocp_node") | .instances[].attributes.name' terraform.tfstate > /tmp/res
 
 #fetch agents in baremetal infra env
 oc get agent -n baremetal -o=jsonpath='{range .items[*]}{.metadata.name}{";"}{.status.inventory.interfaces[*].ipV4Addresses}{";"}{.spec.clusterDeploymentName.name}{";"}{.spec.approved}{";"}{.spec.hostname}{"\n"}{end}' > /tmp/agents
@@ -12,7 +16,7 @@ oc get agent -n baremetal -o=jsonpath='{range .items[*]}{.metadata.name}{";"}{.s
 
 for node in `cat /tmp/res`; do 
   # fetch ip from terraform.tfstate
-  ip=`jq -r '.resources[] | select(.name=="ocp_node") | .instances[] | select(.attributes.name=="'$node'") | .attributes.guest_ip_addresses[]' terraform.tfstate; done | grep 10.6.82`
+  ip=`./jq -r '.resources[] | select(.name=="ocp_node") | .instances[] | select(.attributes.name=="'$node'") | .attributes.guest_ip_addresses[]' terraform.tfstate; done | grep 10.6.82`
   if [[ "$ip" == "" ]]; then
      echo "ip not found for node $node"
      exit 1
