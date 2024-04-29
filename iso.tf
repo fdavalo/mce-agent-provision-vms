2b553b3d167b9d93a190d270d8ec1de637d18d18
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 08:29:23 +0000

    Update iso.tf

--- iso.tf
+++ iso.tf
@@ -1,7 +1,20 @@
-resource "vsphere_file" "ubuntu_vmdk_upload" {
-  datacenter         = "dc-01"
-  datastore          = "datastore-01"
+provider "vsphere" {
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+variable "instance_name" {
+ type = string
+ default = "ocp"
+ description = "instance name"
+}
+
+resource "vsphere_file" "iso_upload" {
+  datacenter         = "HIT_RedHat"
+  datastore          = "VV.HIT-RedHat"
   source_file        = "/my/src/path/custom_ubuntu.vmdk"
-  destination_file   = "/my/dst/path/custom_ubuntu.vmdk"
-  create_directories = true
+  destination_file   = "/ISOs/hcp02.iso"
+  create_directories = false
+}
+
 }
