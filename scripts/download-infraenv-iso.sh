cc8dbeb4b310c7fa97a6faa5fb207a774dd8966c
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 10:20:19 +0000

    Update download-infraenv-iso.sh

--- scripts/download-infraenv-iso.sh
+++ scripts/download-infraenv-iso.sh
@@ -10,4 +10,4 @@ chmod +x jq
 url=`oc get infraenv ${cluster} -n hcp-config -o=json | ./jq -r .status.isoDownloadURL`
 
 #download iso
-curl -k -o iso/infraenv.iso $url
+curl -k -o infraenv.iso $url
