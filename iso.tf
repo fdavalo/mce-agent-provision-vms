35d6f12fc426d9b52d928fd5c93b27a8ab89bf34
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 08:30:28 +0000

    Update iso.tf

--- iso.tf
+++ iso.tf
@@ -5,7 +5,7 @@ provider "vsphere" {
 
 variable "instance_name" {
  type = string
- default = "ocp"
+ default = "hcp02"
  description = "instance name"
 }
 
@@ -13,7 +13,7 @@ resource "vsphere_file" "iso_upload" {
   datacenter         = "HIT_RedHat"
   datastore          = "VV.HIT-RedHat"
   source_file        = "/my/src/path/custom_ubuntu.vmdk"
-  destination_file   = "/ISOs/hcp02.iso"
+  destination_file   = "/ISOs/${lower(var.instance_name)}.iso"
   create_directories = false
 }
 
