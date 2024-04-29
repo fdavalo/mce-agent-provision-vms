94274bc0712c40adb98cca3b413bc1b2e2d5c7cc
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 09:10:08 +0000

    Update iso.tf

--- iso/iso.tf
+++ iso/iso.tf
@@ -12,7 +12,7 @@ variable "instance_name" {
 resource "vsphere_file" "iso_upload" {
   datacenter         = "HIT_RedHat"
   datastore          = "VV.HIT-RedHat"
-  source_file        = "/my/src/path/custom_ubuntu.vmdk"
+  source_file        = "infraenv.iso"
   destination_file   = "/ISOs/${lower(var.instance_name)}.iso"
   create_directories = false
 }
