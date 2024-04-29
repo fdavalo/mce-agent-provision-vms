477702f8232d79d16bc477ae6ebe0a2843cdb92b
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 09:09:39 +0000

    Update download-infraenv-iso.sh

--- scripts/download-infraenv-iso.sh
+++ scripts/download-infraenv-iso.sh
@@ -1,1 +1,13 @@
-isoDownloadURL
+set -x
+
+cluster=$1
+
+#install jq
+curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
+chmod +x jq
+
+#fetch url
+url=`oc get infraenv ${cluster} -n ${cluster}-config -o=json | jq .status.isoDownloadURL`
+
+#download iso
+curl -k -o infraenv.iso "$url"
