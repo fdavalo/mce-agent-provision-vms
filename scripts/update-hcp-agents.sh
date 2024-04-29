af8b4927650d6c28ca1f7253498fa343460c6fca
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 12:25:45 +0000

    Update update-hcp-agents.sh

--- scripts/update-hcp-agents.sh
+++ scripts/update-hcp-agents.sh
@@ -1,15 +1,17 @@
 set -x
 
+cluster=$1
+
 #install jq
 curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
 chmod +x jq
 
 #fetch nodes from terraform.tfstate
-./jq -r '.resources[] | select(.name=="ocp_node") | .instances[].attributes.name' terraform.tfstate > /tmp/res
+./jq -r '.resources[] | select(.name=="'${cluster}'_node") | .instances[].attributes.name' terraform.tfstate > /tmp/res
 
 for node in `cat /tmp/res`; do 
   # fetch ip from terraform.tfstate
-  ip=`./jq -r '.resources[] | select(.name=="ocp_node") | .instances[] | select(.attributes.name=="'$node'") | .attributes.guest_ip_addresses[]' terraform.tfstate | grep 10.6.82`
+  ip=`./jq -r '.resources[] | select(.name=="'${cluster}'_node") | .instances[] | select(.attributes.name=="'$node'") | .attributes.guest_ip_addresses[]' terraform.tfstate | grep 10.6.82`
   if [[ "$ip" == "" ]]; then
      echo "ip not found for node $node"
      exit 1
