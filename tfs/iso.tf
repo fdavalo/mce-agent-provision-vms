13163425efbbae71362ed7d09184cefc115a30f2
fdavalo <57140104+fdavalo@users.noreply.github.com>
2024-04-29 10:24:28 +0000

    Create iso.tf

---
+++ tfs/iso.tf
@@ -0,0 +1,18 @@
+provider "vsphere" {
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+variable "instance_name" {
+ type = string
+ default = "hcp02"
+ description = "instance name"
+}
+
+resource "vsphere_file" "iso_upload" {
+  datacenter         = "HIT_RedHat"
+  datastore          = "VV.HIT-RedHat"
+  source_file        = "infraenv.iso"
+  destination_file   = "/ISOs/${lower(var.instance_name)}.iso"
+  create_directories = false
+}
